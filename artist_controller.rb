b5a4027c1daaabd8cde9d8f877e62eac6b576899
Harjit <harjitk89@hotmail.co.uk>
2018-06-11 00:19:20 +0100

    applied GET methods to artist_controller.rb

--- artist_controller.rb
+++ artist_controller.rb
@@ -3,3 +3,15 @@ require('sinatra/contrib/all')
 require_relative('models/artist')
 require_relative('models/exhibit')
 also_reload('./models/*')
+
+get '/artists' do
+  @artists = Artist.all()
+  erb (:index)
+end
+
+#display a list of artists
+
+get '/artists/new' do
+  @artists = Artist.all()
+  erb(:new)
+end
